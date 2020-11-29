319b0eac507643b93db0438b0f6a20bc7632a5a5
Nathan Tseng <tsnathan@umich.edu>
2020-11-29 12:06:41 -0500

    push datasets

---
+++ datasets/resize.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os, sys
+import argparse
+from pathlib import Path
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--dir', default='road2icy/trainB/')
+arg_parser.add_argument('--size', default=256, type=int)
+opt = arg_parser.parse_args()
+
+
+def resize(opt):
+    path = opt.dir
+    dirs = os.listdir( path )
+    output_dir = opt.dir[:-1]
+    output_dir = Path(output_dir + "-" + str(opt.size)) / "trainA"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    i = 0
+    print(str(output_dir))
+    for item in dirs:
+        print(path+item)
+        if os.path.isfile(path+item):
+            print(i)
+            i = i+1
+            im = Image.open(path+item).convert("RGB")
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((opt.size, opt.size), Image.ANTIALIAS)
+            imResize.save(str(output_dir) + "/" + str(i) + '.jpg', 'JPEG', quality=90)
+
+resize(opt)
